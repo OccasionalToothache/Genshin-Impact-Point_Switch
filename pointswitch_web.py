de28f2797f7a90f8d52155c0b7a5bbde1af5c01f
GC-Ruth <107023226+GC-Ruth@users.noreply.github.com>
2023-03-07 19:01:12 +0800

    Add files via upload

--- pointswitch_web.py
+++ pointswitch_web.py
@@ -2,10 +2,12 @@ import sys
 import requests
 import sqlite3
 import os
+import json
 import qrcode
 import shutil
 import configparser
 import socket
+import atexit
 from pywebio import start_server
 from pywebio import config
 from pywebio.input import *
@@ -13,6 +15,7 @@ from pywebio.output import *
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
+
 
 
 def main():
@@ -24,42 +27,55 @@ def main():
     put_button(label='怪物类', onclick=lambda: GetPoints('怪物类')).style('text-align:center')
     put_button(label='锄地类', onclick=lambda: GetPoints('锄地类')).style('text-align:center')
     put_button(label='清空点位', onclick=lambda: Clearpoint()).style('text-align:center')
-    put_button(label='切换服务器', onclick=lambda: GameTypeChange()).style('text-align:center')
 
 
 def ExitWebMode():
-    clear('ROOT')
-    sys.exit()
+    if os.path.exists('./qr.png'):
+        os.remove('./qr.png')
 
 
 def GetPoints(Pointstype):
     clear('ROOT')
-    db = requests.get('http://www.pyforme.fun/Pt.db').content
-    open('./Pt.db', 'wb').write(db)
-    conn = sqlite3.connect('Pt.db')
+    if not os.path.exists(f'./databases/{Pointstype}.db'):
+        db = requests.get(f'http://www.pyforme.fun/{Pointstype}.db').content
+        open(f'./databases/{Pointstype}.db', 'wb').write(db)
+    conn = sqlite3.connect(f'./databases/{Pointstype}.db')
     cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM {Pointstype}')
-    names = cursor.fetchall()
-    selection = select(label='点位名称', options=names)
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    forms = cursor.fetchall()
+    i = []
+    for form in forms:
+        i.append(form[0])
+    selection = select(label='点位名称', options=i)
     config = configparser.ConfigParser()
     config.read('config.ini')
-    response = requests.get(url=selection).content
-    open('data.zip', 'wb').write(response)
-    if os.path.exists(f'{config["DEFAULT"]["cheatpath"]}/teleports'):
-        shutil.rmtree(f'{config["DEFAULT"]["cheatpath"]}/teleports')
-    shutil.unpack_archive(filename='data.zip', extract_dir=f'{config["DEFAULT"]["cheatpath"]}/teleports')
-    os.remove('data.zip')
-    toast('切换成功！')
-    cursor.close()
+    cheat = config['DEFAULT']['cheat']
+    cheatpath = os.path.dirname(cheat)
+    if not os.path.exists(f'{cheatpath}/teleports'):
+        os.mkdir(f'{cheatpath}/teleports')
+    else:
+        shutil.rmtree(f'{cheatpath}/teleports')
+        os.mkdir(f'{cheatpath}/teleports')
+    conn = sqlite3.connect(f'./databases/{Pointstype}.db')
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT 名称, 点位数据 FROM {selection}")
+    rows = cursor.fetchall()
+    for row in rows:
+        name = row[0]
+        data = json.loads(row[1])
+        filename = name + '.json'
+        with open(f'{cheatpath}/teleports/{filename}', 'w') as f:
+            json.dump(data, f)
     conn.close()
-    os.remove('Pt.db')
+    toast('切换成功！')
     main()
 
 
 def Clearpoint():
     config = configparser.ConfigParser()
     config.read('config.ini')
-    cheatpath = config['DEFAULT']['cheatpath']
+    cheat = config['DEFAULT']['cheat']
+    cheatpath = os.path.dirname(cheat)
     if not os.path.exists(f'{cheatpath}/teleports'):
         os.mkdir(f'{cheatpath}/teleports')
     else:
@@ -67,47 +83,6 @@ def Clearpoint():
         os.mkdir(f'{cheatpath}/teleports')
     toast('清空完毕')
     main()
-
-
-def GameTypeChange():
-    clear('ROOT')
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    gamepath = config['DEFAULT']['gamepath']
-    typelist = ['官服转b服', 'b服转官服', '官服转国际服', '国际服转官服']
-    Value = select(label='请选择转换的服务器', options=typelist)
-    if Value == '官服转b服':
-        shutil.unpack_archive(filename='./data/官转b.zip', extract_dir=gamepath)
-        os.remove(f'{gamepath}/YuanShen_Data/Plugins/metakeeper.dll')
-        toast('已成功从官服转为b服')
-        main()
-    elif Value == 'b服转官服':
-        shutil.unpack_archive(filename='./data/b转官.zip', extract_dir=gamepath)
-        os.remove(f'{gamepath}/YuanShen_Data/Plugins/PCGameSDK.dll')
-        toast('已成功从b服转为官服')
-        main()
-    elif Value == '官服转国际服':
-        if not os.path.exists('./data/官转国3.4.zip'):
-            toast('未检测到官转国依赖包,请前往下载')
-            main()
-        else:
-            put_loading('border', 'dark').style('position: relative;top: 200px;left: 170px')
-            os.rename(f'{gamepath}/YuanShen_Data', f'{gamepath}/GenshinImpact_Data')
-            os.remove(f'{gamepath}/YuanShen.exe')
-            shutil.unpack_archive(filename='./data/官转国3.4.zip', extract_dir=gamepath)
-            toast('已成功从官服转为国际服')
-            main()
-    elif Value == '国际服转官服':
-        if not os.path.exists('./data/国转官3.4.zip'):
-            toast('为检测到国转官依赖包,请前往下载')
-            main()
-        else:
-            put_loading('border', 'dark').style('position: relative;top: 200px;left: 170px')
-            os.rename(f'{gamepath}/GenshinImpact_Data', f'{gamepath}/YuanShen_Data')
-            os.remove(f'{gamepath}/GenshinImpact.exe')
-            shutil.unpack_archive(filename='./data/国转官3.4.zip', extract_dir=gamepath)
-            toast('已成功从国际服转为官服')
-            main()
 
 
 def qr():
@@ -118,6 +93,7 @@ def qr():
     img.save('qr.png')
 
 
+atexit.register(ExitWebMode)
 qr()
-config(title='Pointswitch', theme='yeti')
-# start_server(main, port=8080, debug=True)
+config(title='Pointswitch', theme='sketchy')
+start_server(main, port=8080, debug=True)
