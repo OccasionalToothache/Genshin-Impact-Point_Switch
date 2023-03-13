af18efc16fcdacc2bac97a2468860117ad261e8a
GC-Ruth <107023226+GC-Ruth@users.noreply.github.com>
2023-03-13 19:45:08 +0800

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -2,39 +2,31 @@ import re
 import requests
 import cv2
 import numpy as np
-import win32gui
-import win32con
-import pyzbar.pyzbar as pyzbar
 from PIL import ImageGrab
 from tkinter import messagebox
 
 
 def main(account):
     if account:
+        detector = cv2.QRCodeDetector()
         WINDOW_NAME = "Scanner"
         SCAN_AREA = (700, 400, 1200, 800)
         cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_TOPMOST, 1)
         cv2.resizeWindow(WINDOW_NAME, 500, 400)
-        hwnd = win32gui.FindWindow(None, WINDOW_NAME)
-        win32gui.SetWindowText(hwnd, "扫描窗口")
-        win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
         while True:
             screen = np.array(ImageGrab.grab(bbox=SCAN_AREA))
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
-            decoded_objects = pyzbar.decode(screen)
-            if decoded_objects:
-                for obj in decoded_objects:
-                    x, y, w, h = obj.rect
-                    cv2.rectangle(screen, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+            data, bbox, _ = detector.detectAndDecode(gray)
+            if data:
+                break
             cv2.imshow(WINDOW_NAME, screen)
             if cv2.waitKey(1) == 27:
                 break
-            if decoded_objects:
-                url = re.findall(r"'(.*?)'", str(decoded_objects[0].data))[0]
-                ticket = re.findall(r"ticket=(.*)", str(url))[0]
-                break
         cv2.destroyAllWindows()
-        if decoded_objects:
+        if data:
+            ticket = re.findall(r'ticket=(.*)', data)[0]
             datas = open(f'./scanner/{account}').readlines()
             uid = re.findall(r'uid:(.*)\n', datas[0])[0]
             device = re.findall(r'device:(.*)\n', datas[1])[0]
@@ -51,3 +43,7 @@ def main(account):
                 messagebox.showerror(title='错误', message='请求超时,请检查网络或关闭vpn代理')
     else:
         messagebox.showwarning(title='提示', message='请勾选扫码模式并切换一次账号')
+
+
+# if __name__ == '__main__':
+#     main('大号-官服-S')
